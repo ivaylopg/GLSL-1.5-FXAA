6b6d27fad0aaf7e87c2fb1edae89357e06842c12
Ivaylo Getov <contact@ivaylogetov.com>
2014-07-30 00:04:50 -0400

    comments

--- bin/data/shadersGL3/fxaa.vert
+++ bin/data/shadersGL3/fxaa.vert
@@ -1,5 +1,16 @@
 #version 150
 
+//
+// Implementation of FXAA for GLSL 1.5
+// See: https://github.com/mrdoob/three.js/blob/master/examples/js/shaders/FXAAShader.js
+//      https://github.com/neilmendoza/ofxPostProcessing/blob/master/src/FxaaPass.cpp
+//      http://www.glge.org/demos/fxaa/
+//
+// Ivaylo Getov
+// www.ivaylogetov.com
+//
+
+
 uniform mat4 modelViewProjectionMatrix;
 uniform mat4 textureMatrix;
 
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,5 +1,17 @@
 #include "ofApp.h"
 
+
+//
+// Implementation of FXAA for GLSL 1.5
+// See: https://github.com/mrdoob/three.js/blob/master/examples/js/shaders/FXAAShader.js
+//      https://github.com/neilmendoza/ofxPostProcessing/blob/master/src/FxaaPass.cpp
+//      http://www.glge.org/demos/fxaa/
+//
+// Ivaylo Getov
+// www.ivaylogetov.com
+//
+
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     
